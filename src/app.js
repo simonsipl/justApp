ff9d268e86169390b213f27d0de7a677e1dfba40
Stanisz Szymon <stanisz.szymon@gmail.com>
2019-09-30 22:44:51 +0200

    Express + react connection (#1)

--- src/app.js
+++ src/app.js
@@ -1,17 +1,27 @@
 const express = require('express');
+const path = require('path');
 const { clientError, serverError } = require('./errors/error');
 
 const userConfig = require('./config/userConfig');
 
 module.exports = (connection) => {
   const app = express();
-
   app.use(express.json());
+  if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+  app.get('/api', (req, res) => {
+    res.send({
+      welcome: 'Hello your express + React app is working',
+      someOtherResponse: 'I it just for learning purpose'
+    });
+  });
 
-  app.use('/user', userConfig(connection));
+  app.use('/api/user', userConfig(connection));
 
-  app.get('/', (req, res) => {
-    res.send('root route');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
   });
 
   app.use(clientError);
